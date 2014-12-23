268bdb55e3cd2576b6de6adb2212ddd5e994a63c
Jimmy Man <tranduyminhman@gmail.com>
2014-12-23 22:36:49 +0700

    remove theme twenty fifteen

--- source/wp-content/themes/edex/functions.php
+++ source/wp-content/themes/edex/functions.php
@@ -493,3 +493,15 @@ function edexWheelText($text)
     $text = str_replace("&amp;","&",$text);
     return $text;
 }
+
+
+/*------------------------------------*\
+	Disable updates
+\*------------------------------------*/
+
+function remove_core_updates(){
+    global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
